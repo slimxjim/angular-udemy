3b65b8bdc2d32c09dfe1388b14f1f8a294f11604
Jan Musílek <slimxjim@hmail.com>
2023-03-30 00:02:24 +0200

    23 selector variant

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  selector: '[app-servers]',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
